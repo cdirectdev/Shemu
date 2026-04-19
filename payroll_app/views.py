dff8f78ec7820c278c858ed3f2c18838ac3d9896
cedd <cedric.ross.alipis@student.ateneo.edu>
2026-04-19 12:10:46 +0800

    initial homepage and base

--- payroll_app/views.py
+++ payroll_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Employee
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'payroll_app/home.html')
+    all_employees = Employee.objects.all()
+    return render(request, 'payroll_app/home.html', {'employees': all_employees})
