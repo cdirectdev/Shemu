44665b3da888c4086f31ea74704adea6bba42295
Kielpogi <bejasa.kielvinnie@gmail.com>
2026-04-27 23:41:34 +0800

    views.py

--- payroll_app/views.py
+++ payroll_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from .models import Employee
+from django.shortcuts import render, redirect, get_object_or_404   
+from .models import Employee, Payslip
 
 # Create your views here.
 
